f18a2b803447e3855b393c271b42fa3de61855fa
Oliver0804 <icetzsr@gmail.com>
2020-11-15 22:14:42 +0800

    fix detection funs

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,40 +1,40 @@
 /**
-  ******************************************************************************
-  * File Name          : main.c
-  * Description        : Main program body
-  ******************************************************************************
-  ** This notice applies to any and all portions of this file
-  * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether
-  * inserted by the user or by software development tools
-  * are owned by their respective copyright owners.
-  *
-  * COPYRIGHT(c) 2020 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * File Name          : main.c
+ * Description        : Main program body
+ ******************************************************************************
+ ** This notice applies to any and all portions of this file
+ * that are not between comment pairs USER CODE BEGIN and
+ * USER CODE END. Other portions of this file, whether
+ * inserted by the user or by software development tools
+ * are owned by their respective copyright owners.
+ *
+ * COPYRIGHT(c) 2020 STMicroelectronics
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ */
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -61,7 +61,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 uint32_t runtime = 6000;
-uint32_t sensitivity = 50;
+uint32_t sensitivity = 20;
 
 //uint32_t timecount=0;
 uint32_t stage = 0;
@@ -73,9 +73,11 @@ int settingMode = 0;
 
 int i;
 uint32_t ADC_Value[100];
+int maxLoadCount = 0;
 uint32_t ad1, ad2;
 uint32_t real_adc1, real_adc2;
 uint32_t keep_adc1, keep_adc2;
+uint32_t maxLoad = 0;
 uint32_t PWM1 = 0, PWM2 = 0;
 uint32_t OUTPUT_1_State = 0, OUTPUT_2_State = 0;
 uint32_t sysinfo_State = 0, button_State = 0;
@@ -150,15 +152,16 @@ void clean_button_flag(void) {
 	}
 }
 int read_ADC() {
-	for (i = 0, ad1 = 0, ad2 = 0; i < 10;) {
+	int times = 50;
+	for (i = 0, ad1 = 0, ad2 = 0; i < times;) {
 		ad1 += ADC_Value[i++];
 		ad2 += ADC_Value[i++];
 		//HAL_Delay(1);
 	}
 
-	real_adc1 = ad1 / 5;
-	real_adc2 = ad2 / 5;
-	sensitivity=real_adc2;
+	real_adc1 = ad1 / (times / 2);
+	real_adc2 = ad2 / (times / 2);
+	//sensitivity = real_adc2;
 	if (real_adc1 <= real_adc2) {
 		return 1;
 	} else {
@@ -166,24 +169,32 @@ int read_ADC() {
 	}
 }
 //檢知附載
-int detection_load(int time, int th) {
-	if (time == 0)
-		keep_adc1 = real_adc1;
-	else {
-		if (abs(keep_adc1 - real_adc1) > th) {
-			keep_adc1 = real_adc1;
-			return 1;
-		} else {
-			keep_adc2 = real_adc1;
-			return 0;
+//int time =0 ;為第一次設置
+int detection_load(int times, uint32_t th) {
+	if (times == 0) {
+		//keep_adc1 = real_adc1;
+		maxLoad = 0;
+		maxLoadCount = 0;
+	} else {
+		maxLoadCount++;
+		if (maxLoadCount < 3) {
+			if (real_adc1 > maxLoad) {
+				maxLoad = real_adc1+th;
+			}
+		}else{
+			if (real_adc1 > (maxLoad)) {
+				//maxLoad = real_adc1;
+				return 1;
+			}
 		}
 	}
+	return 0;
 }
 
 void Display(int mode) {
 	ssd1306_Fill(Black);
 	ssd1306_SetCursor(2, 0);
-	int line_count=1;
+	int line_count = 1;
 	if (mode == 0) {
 
 		snprintf(buff, sizeof(buff), "%s,%s", __DATE__, __TIME__);
@@ -197,7 +208,8 @@ void Display(int mode) {
 		ssd1306_SetCursor(2, 8 * line_count++);
 		ssd1306_WriteString(buff, Font_6x8, White);
 
-		snprintf(buff, sizeof(buff), "[A7]:%d,KA2:%d", real_adc2, keep_adc2);
+		snprintf(buff, sizeof(buff), "mLoad:%d count:%d", maxLoad,
+				maxLoadCount);
 		ssd1306_SetCursor(2, 8 * line_count++);
 		ssd1306_WriteString(buff, Font_6x8, White);
 
@@ -205,9 +217,12 @@ void Display(int mode) {
 		ssd1306_SetCursor(2, 8 * line_count++);
 		ssd1306_WriteString(buff, Font_6x8, White);
 
+
 		snprintf(buff, sizeof(buff), "PWM2:%d, GPIO2:%d", PWM2, OUTPUT_2_State);
 		ssd1306_SetCursor(2, 8 * line_count++);
 		ssd1306_WriteString(buff, Font_6x8, White);
+
+
 
 		snprintf(buff, sizeof(buff), "button[%d]:%d,%d,%d,%d", button_State,
 				button_flag[1], button_flag[2], button_flag[3], button_flag[4]);
@@ -222,7 +237,7 @@ void Display(int mode) {
 			ssd1306_SetCursor(2, 8 * 7);
 			ssd1306_WriteString(buff, Font_6x8, White);
 		}
-	}else if(mode ==1){//setting mode
+	} else if (mode == 1) {		//setting mode
 		snprintf(buff, sizeof(buff), "Setting mode.");
 		ssd1306_WriteString(buff, Font_6x8, White);
 
@@ -308,39 +323,38 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 }
 /* USER CODE END 0 */
 
-int main(void)
-{
+int main(void) {
 
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration----------------------------------------------------------*/
+	/* MCU Configuration----------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_I2C1_Init();
-  MX_ADC1_Init();
-  MX_TIM2_Init();
-  MX_RTC_Init();
-  MX_USART2_UART_Init();
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_DMA_Init();
+	MX_I2C1_Init();
+	MX_ADC1_Init();
+	MX_TIM2_Init();
+	MX_RTC_Init();
+	MX_USART2_UART_Init();
 
-  /* USER CODE BEGIN 2 */
+	/* USER CODE BEGIN 2 */
 	//ssd1306_Reset();
 	ssd1306_Init();
 	HAL_ADC_Start_DMA(&hadc1, (uint32_t*) &ADC_Value, 100);
@@ -348,15 +362,14 @@ int main(void)
 	motor_control(0, 0);
 	//ssd1306_TestLine();
 	//ssd1306_TestAll();
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1) {
-  /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-  /* USER CODE BEGIN 3 */
-		//HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+		/* USER CODE BEGIN 3 */
 		//系統運行時間
 		start = HAL_GetTick();
 		if (start < end) {	//時間未結束
@@ -367,8 +380,9 @@ int main(void)
 						last_stage = stage;
 						detection_load(0, sensitivity);
 					}
-					if (detection_load(1, sensitivity))
+					if (detection_load(1, sensitivity)) {
 						sysinfo_State = 1;
+					}
 					motor_control(1, 1000);
 				} else if (start < end - 2000) {
 					stage = 2;
@@ -376,8 +390,9 @@ int main(void)
 						last_stage = stage;
 						detection_load(0, sensitivity);
 					}
-					if (detection_load(1, sensitivity))
+					if (detection_load(1, sensitivity)) {
 						sysinfo_State = 1;
+					}
 					motor_control(1, 4000);
 				} else if (start < end) {
 					stage = 3;
@@ -385,8 +400,9 @@ int main(void)
 						last_stage = stage;
 						detection_load(0, sensitivity);
 					}
-					if (detection_load(1, sensitivity))
+					if (detection_load(1, sensitivity)) {
 						sysinfo_State = 1;
+					}
 					motor_control(1, 1000);
 				}
 			} else if (dir_flag == 4) {
@@ -396,8 +412,9 @@ int main(void)
 						last_stage = stage;
 						detection_load(0, sensitivity);
 					}
-					if (detection_load(1, sensitivity))
+					if (detection_load(1, sensitivity)) {
 						sysinfo_State = 1;
+					}
 					motor_control(-1, 1000);
 				} else if (start < end - 2000) {
 					stage = 2;
@@ -405,8 +422,9 @@ int main(void)
 						last_stage = stage;
 						detection_load(0, sensitivity);
 					}
-					if (detection_load(1, sensitivity))
+					if (detection_load(1, sensitivity)) {
 						sysinfo_State = 1;
+					}
 					motor_control(-1, 4000);
 				} else if (start < end) {
 					stage = 3;
@@ -414,8 +432,9 @@ int main(void)
 						last_stage = stage;
 						detection_load(0, sensitivity);
 					}
-					if (detection_load(1, sensitivity))
+					if (detection_load(1, sensitivity)) {
 						sysinfo_State = 1;
+					}
 					motor_control(-1, 1000);
 				}
 			}
@@ -446,8 +465,8 @@ int main(void)
 		} else if (button_State == 5) {
 			motor_point(4000);
 			settingMode = 1;
-		}else if(button_State==8){
-			settingMode=0;
+		} else if (button_State == 8) {
+			settingMode = 0;
 		}
 
 		//LCD畫面
@@ -458,71 +477,68 @@ int main(void)
 		}
 
 		//系統LED PC13
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+		//HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
 
 		//清除button flag
 		clean_button_flag();
 	}
 
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 
 }
 
 /** System Clock Configuration
-*/
-void SystemClock_Config(void)
-{
+ */
+void SystemClock_Config(void) {
 
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
-    /**Initializes the CPU, AHB and APB busses clocks
-    */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 16;
-  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+	/**Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI
+			| RCC_OSCILLATORTYPE_LSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = 16;
+	RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		_Error_Handler(__FILE__, __LINE__);
+	}
 
-    /**Initializes the CPU, AHB and APB busses clocks
-    */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV16;
+	/**Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV16;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		_Error_Handler(__FILE__, __LINE__);
+	}
 
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC|RCC_PERIPHCLK_ADC;
-  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
-  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV8;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC | RCC_PERIPHCLK_ADC;
+	PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
+	PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV8;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+		_Error_Handler(__FILE__, __LINE__);
+	}
 
-    /**Configure the Systick interrupt time
-    */
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+	/**Configure the Systick interrupt time
+	 */
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
 
-    /**Configure the Systick
-    */
-  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+	/**Configure the Systick
+	 */
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
-  /* SysTick_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+	/* SysTick_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
 /* USER CODE BEGIN 4 */
@@ -530,17 +546,16 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @param  None
-  * @retval None
-  */
-void _Error_Handler(char * file, int line)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @param  None
+ * @retval None
+ */
+void _Error_Handler(char *file, int line) {
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	while (1) {
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef USE_FULL_ASSERT
@@ -564,11 +579,11 @@ void assert_failed(uint8_t* file, uint32_t line)
 #endif
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /**
-  * @}
-*/
+ * @}
+ */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
