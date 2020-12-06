4adf6d6d8073c87445457750355e7199896531f6
Oliver0804 <icetzsr@gmail.com>
2020-12-06 22:26:29 +0800

    增加煞車功能

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,40 +1,40 @@
 /**
-  ******************************************************************************
-  * File Name          : main.c
-  * Description        : Main program body
-  ******************************************************************************
-  ** This notice applies to any and all portions of this file
-  * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether
-  * inserted by the user or by software development tools
-  * are owned by their respective copyright owners.
-  *
-  * COPYRIGHT(c) 2020 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * File Name          : main.c
+ * Description        : Main program body
+ ******************************************************************************
+ ** This notice applies to any and all portions of this file
+ * that are not between comment pairs USER CODE BEGIN and
+ * USER CODE END. Other portions of this file, whether
+ * inserted by the user or by software development tools
+ * are owned by their respective copyright owners.
+ *
+ * COPYRIGHT(c) 2020 STMicroelectronics
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ */
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -99,8 +99,7 @@ char buff[64];
 
 int i2c_working = 1;
 
-
-uint32_t *data = (uint32_t *)"Hello World";
+uint32_t *data = (uint32_t*) "Hello World";
 uint32_t Rx_Data[4];
 char string[30];
 char tempString[30];
@@ -115,6 +114,30 @@ void SystemClock_Config(void);
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
+void motor_stop(int dir) {
+	if (dir >= 1) {
+		OUTPUT_1_State = 1;
+		OUTPUT_2_State = 1;
+		PWM1 = 0;
+		PWM2 = 0;
+		HAL_GPIO_WritePin(OUTPUT_M1_GPIO_Port, OUTPUT_M1_Pin, 1);
+		HAL_GPIO_WritePin(OUTPUT_M2_GPIO_Port, OUTPUT_M2_Pin, 1);
+		user_pwm_setvalue_1(0);
+		user_pwm_setvalue_2(0);
+
+	} else if (dir <= -1) {
+		OUTPUT_1_State = 0;
+		OUTPUT_2_State = 0;
+		PWM1 = 4000;
+		PWM2 = 4000;
+		HAL_GPIO_WritePin(OUTPUT_M1_GPIO_Port, OUTPUT_M1_Pin, 0);
+		HAL_GPIO_WritePin(OUTPUT_M2_GPIO_Port, OUTPUT_M2_Pin, 0);
+		user_pwm_setvalue_1(4000);
+		user_pwm_setvalue_2(4000);
+	}
+	HAL_Delay(100);
+	motor_control(0,0);
+}
 void motor_control(int dir, int pwm) {
 	if (setDir_flag >= 1) {
 		if (dir >= 1) {
@@ -179,36 +202,39 @@ void motor_control(int dir, int pwm) {
 	}
 }
 
-void WriteSet(int dataInt1,int dataInt2,int dataInt3,int dataInt4,int dataInt5,int dataInt6){
+void WriteSet(int dataInt1, int dataInt2, int dataInt3, int dataInt4,
+		int dataInt5, int dataInt6) {
 	//sprintf(*tempString,"%s%s%s%s%s",dataInt1,dataInt2,dataInt3,dataInt4,dataInt5);
-	sprintf(tempString,"%4d%4d%4d%4d%4d%4d",dataInt1,dataInt2,dataInt3,dataInt4,dataInt5,dataInt6);
-	Flash_Write_Data(0x0801FBF8 , tempString);
+	sprintf(tempString, "%4d%4d%4d%4d%4d%4d", dataInt1, dataInt2, dataInt3,
+			dataInt4, dataInt5, dataInt6);
+	Flash_Write_Data(0x0801FBF8, tempString);
 }
-void KeepSet(){
-	WriteSet(nowPosition,stayPositionUp,stayPositionDown,sensitivity,slowValue,setDir_flag);
+void KeepSet() {
+	WriteSet(nowPosition, stayPositionUp, stayPositionDown, sensitivity,
+			slowValue, setDir_flag);
 }
-void ReadSet(){
+void ReadSet() {
 	Flash_Read_Data(0x0801FBF8, Rx_Data);
 	Convert_To_Str(Rx_Data, string);
-	int xx[6]={0};
-	int xflag=0;
-	for(int x=0;x<23;x=x+4){
+	int xx[6] = { 0 };
+	int xflag = 0;
+	for (int x = 0; x < 23; x = x + 4) {
 		char temp[5];
-		temp[0]=string[x];
-		temp[1]=string[x+1];
-		temp[2]=string[x+2];
-		temp[3]=string[x+3];
-		temp[4]='\0';
+		temp[0] = string[x];
+		temp[1] = string[x + 1];
+		temp[2] = string[x + 2];
+		temp[3] = string[x + 3];
+		temp[4] = '\0';
 
-		xx[xflag++]=atoi(temp);
+		xx[xflag++] = atoi(temp);
 		HAL_Delay(1);
 	}
-	nowPosition=xx[0];
-	stayPositionUp=xx[1];
-	stayPositionDown=xx[2];
-	sensitivity=xx[3];
-	slowValue=xx[4];
-	setDir_flag=xx[5];
+	nowPosition = xx[0];
+	stayPositionUp = xx[1];
+	stayPositionDown = xx[2];
+	sensitivity = xx[3];
+	slowValue = xx[4];
+	setDir_flag = xx[5];
 	//Convert_To_Str(Rx_Data, string);
 }
 
@@ -313,9 +339,9 @@ void timerControl() {
 void stepControl() {
 	switch (dir_flag) {
 	case 1:
-		if(nowPosition > stayPositionUp){
+		if (nowPosition > stayPositionUp) {
 			motor_control(1, 1500);
-			HAL_Delay(slowValue*100);
+			HAL_Delay(slowValue * 100);
 		}
 		while (nowPosition > stayPositionUp) {
 			motor_control(1, 4000);
@@ -326,22 +352,24 @@ void stepControl() {
 			for (int timer = 0; timer < 5; timer++) {
 				HAL_Delay(1);
 				Display(settingMode);
-				if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_SET||detection_load(1, sensitivity)) {
+				if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_SET
+						|| detection_load(1, sensitivity)) {
 					dir_flag = 0;
 					break;
 				}
 			}
-			if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_SET||detection_load(1, sensitivity)) {
+			if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_SET
+					|| detection_load(1, sensitivity)) {
 				dir_flag = 0;
 				break;
 			}
 		}
-		motor_control(0, 0);
+		motor_stop(1);
 		break;
 	case 4:
-		if(nowPosition < stayPositionDown){
+		if (nowPosition < stayPositionDown) {
 			motor_control(-1, 1500);
-			HAL_Delay(slowValue*100);
+			HAL_Delay(slowValue * 100);
 		}
 		while (nowPosition < stayPositionDown) {
 			motor_control(-1, 4000);
@@ -352,25 +380,27 @@ void stepControl() {
 			for (int timer = 0; timer < 5; timer++) {
 				HAL_Delay(1);
 				Display(settingMode);
-				if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_SET||detection_load(1, sensitivity)) {
+				if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_SET
+						|| detection_load(1, sensitivity)) {
 					dir_flag = 0;
 					break;
 				}
 			}
-			if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_SET||detection_load(1, sensitivity)) {
+			if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_SET
+					|| detection_load(1, sensitivity)) {
 				dir_flag = 0;
 				break;
 			}
 		}
-		motor_control(0, 0);
+		motor_stop(-1);
 		break;
 	default:
 		//motor_control(0, 0);
 		break;
 	}
 }
-void buzzerTimes(int times){
-	for(int x=0;x<times;x++){
+void buzzerTimes(int times) {
+	for (int x = 0; x < times; x++) {
 		HAL_GPIO_WritePin(GPIOA, OUTPUT_BUZZER_Pin, 1);
 		HAL_Delay(100);
 		HAL_GPIO_WritePin(GPIOA, OUTPUT_BUZZER_Pin, 0);
@@ -631,7 +661,7 @@ int check_buttom() {
 					}
 					if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_SET) {
 						settingMode = 1;
-						sensitivity=sensitivityValue;
+						sensitivity = sensitivityValue;
 						buzzerTimes(3);
 						KeepSet();
 					}
@@ -760,39 +790,38 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 }
 /* USER CODE END 0 */
 
-int main(void)
-{
+int main(void) {
 
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration----------------------------------------------------------*/
+	/* MCU Configuration----------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_I2C1_Init();
-  MX_ADC1_Init();
-  MX_TIM2_Init();
-  MX_RTC_Init();
-  MX_USART2_UART_Init();
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_DMA_Init();
+	MX_I2C1_Init();
+	MX_ADC1_Init();
+	MX_TIM2_Init();
+	MX_RTC_Init();
+	MX_USART2_UART_Init();
 
-  /* USER CODE BEGIN 2 */
+	/* USER CODE BEGIN 2 */
 	//ssd1306_Reset();
 	ssd1306_Init();
 	HAL_ADC_Start_DMA(&hadc1, (uint32_t*) &ADC_Value, 100);
@@ -805,25 +834,25 @@ int main(void)
 	//Flash_Write_Data(0x0801FBF8 , "0000,0000,0000,0000,0000");
 	//Flash_Read_Data(0x0801FBF8, Rx_Data);
 	/*
-	1.nowPosition
-	2.stayPositionUp
-	3.stayPositionDown
-	4.sensitivity
-	5.slowValue
-	6.dir_flag
+	 1.nowPosition
+	 2.stayPositionUp
+	 3.stayPositionDown
+	 4.sensitivity
+	 5.slowValue
+	 6.dir_flag
 
-	*/
+	 */
 	//WriteSet(5,1,20,100,5,1);
 	ReadSet();
 	buzzerTimes(1);
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1) {
-  /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-  /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 		//系統運行時間
 		start = HAL_GetTick();
 		if (settingMode == 0) {	//運行模式
@@ -853,65 +882,62 @@ int main(void)
 		clean_button_flag();
 	}
 
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 
 }
 
 /** System Clock Configuration
-*/
-void SystemClock_Config(void)
-{
+ */
+void SystemClock_Config(void) {
 
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
-    /**Initializes the CPU, AHB and APB busses clocks
-    */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 16;
-  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+	/**Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI
+			| RCC_OSCILLATORTYPE_LSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = 16;
+	RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		_Error_Handler(__FILE__, __LINE__);
+	}
 
-    /**Initializes the CPU, AHB and APB busses clocks
-    */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV16;
+	/**Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV16;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		_Error_Handler(__FILE__, __LINE__);
+	}
 
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC|RCC_PERIPHCLK_ADC;
-  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
-  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV8;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC | RCC_PERIPHCLK_ADC;
+	PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
+	PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV8;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+		_Error_Handler(__FILE__, __LINE__);
+	}
 
-    /**Configure the Systick interrupt time
-    */
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+	/**Configure the Systick interrupt time
+	 */
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
 
-    /**Configure the Systick
-    */
-  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+	/**Configure the Systick
+	 */
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
-  /* SysTick_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+	/* SysTick_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
 /* USER CODE BEGIN 4 */
@@ -919,17 +945,16 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @param  None
-  * @retval None
-  */
-void _Error_Handler(char * file, int line)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @param  None
+ * @retval None
+ */
+void _Error_Handler(char *file, int line) {
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	while (1) {
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef USE_FULL_ASSERT
@@ -953,11 +978,11 @@ void assert_failed(uint8_t* file, uint32_t line)
 #endif
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /**
-  * @}
-*/
+ * @}
+ */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
